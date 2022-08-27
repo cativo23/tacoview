472ab1d1c790434f71f6a3f923689e69a0ca6424
Carlos Cativo <cativo23.kt@gmail.com>
2022-08-26 23:14:17 -0600

    🧑‍💻 Install dev dependencies for better development

--- app/Models/City.php
+++ app/Models/City.php
@@ -5,6 +5,39 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\City
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $state_code
+ * @property string $country_code
+ * @property string $latitude
+ * @property string $longitude
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property \Illuminate\Support\Carbon $updated_at
+ * @property int $flag
+ * @property string|null $wikiDataId Rapid API GeoDB Cities
+ * @property int $state_id
+ * @property int $country_id
+ * @method static \Database\Factories\CityFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|City newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|City newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|City query()
+ * @method static \Illuminate\Database\Eloquent\Builder|City whereCountryCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|City whereCountryId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|City whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|City whereFlag($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|City whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|City whereLatitude($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|City whereLongitude($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|City whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|City whereStateCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|City whereStateId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|City whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|City whereWikiDataId($value)
+ * @mixin \Eloquent
+ */
 class City extends Model
 {
     use HasFactory;
--- app/Models/Country.php
+++ app/Models/Country.php
@@ -2,9 +2,70 @@
 
 namespace App\Models;
 
+use Database\Factories\CountryFactory;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\Country
+ *
+ * @property int $id
+ * @property string $name
+ * @property string|null $iso3
+ * @property string|null $numeric_code
+ * @property string|null $iso2
+ * @property string|null $phonecode
+ * @property string|null $capital
+ * @property string|null $currency
+ * @property string|null $currency_name
+ * @property string|null $currency_symbol
+ * @property string|null $tld
+ * @property string|null $native
+ * @property string|null $region
+ * @property string|null $subregion
+ * @property string|null $timezones
+ * @property string|null $translations
+ * @property string|null $latitude
+ * @property string|null $longitude
+ * @property string|null $emoji
+ * @property string|null $emojiU
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property int $flag
+ * @property string|null $wikiDataId Rapid API GeoDB Cities
+ * @method static CountryFactory factory(...$parameters)
+ * @method static Builder|Country newModelQuery()
+ * @method static Builder|Country newQuery()
+ * @method static Builder|Country query()
+ * @method static Builder|Country whereCapital($value)
+ * @method static Builder|Country whereCreatedAt($value)
+ * @method static Builder|Country whereCurrency($value)
+ * @method static Builder|Country whereCurrencyName($value)
+ * @method static Builder|Country whereCurrencySymbol($value)
+ * @method static Builder|Country whereEmoji($value)
+ * @method static Builder|Country whereEmojiU($value)
+ * @method static Builder|Country whereFlag($value)
+ * @method static Builder|Country whereId($value)
+ * @method static Builder|Country whereIso2($value)
+ * @method static Builder|Country whereIso3($value)
+ * @method static Builder|Country whereLatitude($value)
+ * @method static Builder|Country whereLongitude($value)
+ * @method static Builder|Country whereName($value)
+ * @method static Builder|Country whereNative($value)
+ * @method static Builder|Country whereNumericCode($value)
+ * @method static Builder|Country wherePhonecode($value)
+ * @method static Builder|Country whereRegion($value)
+ * @method static Builder|Country whereSubregion($value)
+ * @method static Builder|Country whereTimezones($value)
+ * @method static Builder|Country whereTld($value)
+ * @method static Builder|Country whereTranslations($value)
+ * @method static Builder|Country whereUpdatedAt($value)
+ * @method static Builder|Country whereWikiDataId($value)
+ * @mixin Eloquent
+ */
 class Country extends Model
 {
     use HasFactory;
